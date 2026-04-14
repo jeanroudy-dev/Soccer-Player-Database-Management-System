370887b34b79d8fb7c4abdd67639ba9762d35226
Jean Roudy Alexis <jalexis17@mail.valenciacollege.edu>
2026-04-13 22:30:53 -0400

    Add files via upload
    Javadoc

---
+++ spdmsdb/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"DatabaseManager"},{"p":"<Unnamed>","l":"PlayerManager"},{"p":"<Unnamed>","l":"SoccerPlayer"},{"p":"<Unnamed>","l":"SoccerPlayerGUI"}];updateSearchResults();
